112e16feeabe84a5bd71a39c6ce7ecb227d43a09
Chidiebere Okpala <okpalacollins4@gmail.com>
2021-04-11 11:03:09 -0600

    Variables in Templates

--- 006_variable/01/main.go
+++ 006_variable/01/main.go
@@ -13,6 +13,7 @@ func init() {
 }
 
 func main() {
+	//We passed a string as data instea dof an int for the previous. In inside our template file we assigned a variable, then used te variable
 	err := tpl.ExecuteTemplate(os.Stdout, "tpl.gohtml", `Release self-focus; embrace other-focus.`)
 	if err != nil {
 		log.Fatalln(err)
