6ac5a8df6f9c781f728eff315f76aef8c4e5bdf2
Chidiebere Okpala <okpalacollins4@gmail.com>
2021-04-11 10:53:33 -0600

    Passing data into a templates

--- 005_data/01/main.go
+++ 005_data/01/main.go
@@ -13,6 +13,13 @@ func init() {
 }
 
 func main() {
+	/*
+		Rememeber in the previous programs before this point, we use to pass nil
+		in the third argument which is for data. Now we pass in 42. The 42 will be populated in the
+		"{{.}}" of the template file(tpl.gohtml).
+
+		You only get to pass in one pice of data though. The data can be of type slice, map or another struct
+	*/
 	err := tpl.ExecuteTemplate(os.Stdout, "tpl.gohtml", 42)
 	if err != nil {
 		log.Fatalln(err)
