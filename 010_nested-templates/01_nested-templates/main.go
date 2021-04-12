e0e38666aafaed1f0363f61927a3c9b495623cf5
Chidiebere Okpala <okpalacollins4@gmail.com>
2021-04-12 16:26:59 -0600

    Nesting Templates - How to abstract and modularize templates
    Basically have different parts of a code that are templates and bring them all together. This is know as Nested templates

--- 010_nested-templates/01_nested-templates/main.go
+++ 010_nested-templates/01_nested-templates/main.go
@@ -8,6 +8,15 @@ import (
 
 var tpl *template.Template
 
+/*
+Nothing really changes in the go file from what you dont already know
+like Parsing your template files, Executing them and passing data to them
+
+The only thinng new now is that some of your template file are now being used inside other files!
+
+Some template file just contain logic of you defining them!
+*/
+
 func init() {
 	tpl = template.Must(template.ParseGlob("templates/*.gohtml"))
 }
