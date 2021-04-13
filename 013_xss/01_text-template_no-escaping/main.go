0c59c745e22e8b5be35fafe76e35af0772d37566
Chidiebere Okpala <okpalacollins4@gmail.com>
2021-04-12 19:22:34 -0600

    Using package html/template, character escaping, & cross-site scripting
    HTML Template package is built on top of Text template
    
    In Real projects, if you were to make webpages with go, use the html template package over text template!

--- 013_xss/01_text-template_no-escaping/main.go
+++ 013_xss/01_text-template_no-escaping/main.go
@@ -18,12 +18,13 @@ func init() {
 	tpl = template.Must(template.ParseFiles("tpl.gohtml"))
 }
 
+//An exampe of using a Text template to show how it does not prevent Cross Site Scripting
 func main() {
 
 	home := Page{
 		Title:   "Nothing Escaped",
 		Heading: "Nothing is escaped with text/template",
-		Input:   `<script>alert("Yow!");</script>`,
+		Input:   `<script>alert("Yow!");</script>`, //This right here will surley execut in our code!
 	}
 
 	err := tpl.ExecuteTemplate(os.Stdout, "tpl.gohtml", home)
--- 013_xss/02_html-template_escaping/main.go
+++ 013_xss/02_html-template_escaping/main.go
@@ -1,7 +1,7 @@
 package main
 
 import (
-	"html/template"
+	"html/template" //NOTE here we now use html package instead of Text template. All the functions that exists in text template still carries over here. Nothing new to learn. But this one works better if you are actually building real web pages for your site over the text/template package!
 	"log"
 	"os"
 )
@@ -23,11 +23,17 @@ func main() {
 	home := Page{
 		Title:   "Escaped",
 		Heading: "Danger is escaped with html/template",
-		Input:   `<script>alert("Yow!");</script>`,
+		Input:   `<script>alert("Yow!");</script>`, //This will not execute
 	}
 
 	err := tpl.ExecuteTemplate(os.Stdout, "tpl.gohtml", home)
 	if err != nil {
 		log.Fatalln(err)
 	}
+
+	/*
+		https://pkg.go.dev/html/template has more additional functionalities for doing
+		html pages. It has a lot of safety for preventing web attacks like Cross-Site Scripting.
+		So it escapes a lot of characters
+	*/
 }
