787d6f004b967be3e31c4368588da2c8ae9a22ab
Chidiebere Okpala <okpalacollins4@gmail.com>
2021-04-13 17:19:27 -0600

    Understanding and Usig ListenAndServe
    REMEMBER: We dont have to use the net package anymore to set up our server. The just use the net/http package to set up our server!

--- 017_understanding-net-http-package/02_listenAndServe/main.go
+++ 017_understanding-net-http-package/02_listenAndServe/main.go
@@ -7,11 +7,14 @@ import (
 
 type hotdog int
 
+//We know hotdog is a Handler type as well
 func (m hotdog) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	//Inside this function, I can write back as respnse any code you want
+	//But for this program we just print this to the web when the user goes to http://localhost:8080/
 	fmt.Fprintln(w, "Any code you want in this func")
 }
 
 func main() {
-	var d hotdog
-	http.ListenAndServe(":8080", d)
+	var d hotdog                    //create a variable of type hotdog
+	http.ListenAndServe(":8080", d) //pass thatvariable to ListenAndServe. This is not a HTTPS connection. If you want HTTPS look at the README file
 }
