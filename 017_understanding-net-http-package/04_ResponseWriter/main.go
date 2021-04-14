63c3dba2ef02393308d034b610a67c8a5d17eafa
Chidiebere Okpala <okpalacollins4@gmail.com>
2021-04-14 14:13:09 -0600

    Exploring http.ResponseWriter type - writing headers to the response
    The ResponseWriter is not a struct this time but an interface. https://golang.org/pkg/net/http/#ResponseWriter
    So any type with the three methods is a ResponseWriter

--- 017_understanding-net-http-package/04_ResponseWriter/main.go
+++ 017_understanding-net-http-package/04_ResponseWriter/main.go
@@ -8,7 +8,8 @@ import (
 type hotdog int
 
 func (m hotdog) ServeHTTP(w http.ResponseWriter, req *http.Request) {
-	w.Header().Set("Mcleod-Key", "this is from mcleod")
+	//We are basically setting any header we want
+	w.Header().Set("Mcleod-Key", "this is from mcleod") //type Header has a lot of function attached to it. But we use Set() method to set headers that will be returned by the server https://golang.org/pkg/net/http/#Header
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	fmt.Fprintln(w, "<h1>Any code you want in this func</h1>")
 }
