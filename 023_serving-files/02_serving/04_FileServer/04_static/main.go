148a35b8d6d240ded11adf84eb60664d44db12f2
Chidiebere Okpala <okpalacollins4@gmail.com>
2021-04-18 16:06:35 -0600

    Creating a static file server with http.FileServer

--- 023_serving-files/02_serving/04_FileServer/04_static/main.go
+++ 023_serving-files/02_serving/04_FileServer/04_static/main.go
@@ -8,3 +8,16 @@ import (
 func main() {
 	log.Fatal(http.ListenAndServe(":8080", http.FileServer(http.Dir("."))))
 }
+
+/*
+The code here is thesame to
+
+    http.Handle("/", http.FileServer(http.Dir(".")))
+	http.ListenAndServe(":8080", nil)
+
+
+NOTE:
+    Becasue we have a file called index.html, it will no longer show our folders or files when we go to root (/)
+	It says that in the documentation https://golang.org/pkg/net/http/#FileServer
+	So index.html is a MUST have to serve your static website successfully
+*/
