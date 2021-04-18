56b019fa5855f35390c1ae86e930574bd4be0537
Chidiebere Okpala <okpalacollins4@gmail.com>
2021-04-18 15:30:08 -0600

    Serving a FIle with http.FIleServer and http.StripPrefix

--- 023_serving-files/02_serving/04_FileServer/02/main.go
+++ 023_serving-files/02_serving/04_FileServer/02/main.go
@@ -7,17 +7,25 @@ import (
 
 func main() {
 	http.HandleFunc("/", dog)
+	/*
+		Any request coming in at the /resources path will use the Handler (http.StripPerfix method)
+
+		http.StripPrefix method strips off "/resources" from the request path "/resources/toby.jpg". So we now have "/toby.jpg"
+
+		The httpFileServer will now look for the image "/toby.jpg". You can image the Dir method looking for your image like http.Dir("./assets/toby.jpg")
+	*/
 	http.Handle("/resources/", http.StripPrefix("/resources", http.FileServer(http.Dir("./assets"))))
 	http.ListenAndServe(":8080", nil)
 }
 
 func dog(w http.ResponseWriter, req *http.Request) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	io.WriteString(w, `<img src="/resources/toby.jpg">`)
+	io.WriteString(w, `<img src="/resources/toby.jpg">`) // /resources/toby.jpg is one of the image served at the path /resources
 }
 
 /*
 
 ./assets/toby.jpg
 
+Your code main.go is outside the assets folder, so it will not get served
 */
--- 023_serving-files/02_serving/04_FileServer/03/main.go
+++ 023_serving-files/02_serving/04_FileServer/03/main.go
@@ -5,6 +5,12 @@ import (
 	"net/http"
 )
 
+/*
+This code here is similar to the code in the /02 folder.
+
+We did not use /assets ins /02 folder so you that you can fully understand what is going on
+as we explained in the /02 folder!
+*/
 func main() {
 	http.HandleFunc("/", dog)
 	http.Handle("/assets/", http.StripPrefix("/assets", http.FileServer(http.Dir("./assets"))))
