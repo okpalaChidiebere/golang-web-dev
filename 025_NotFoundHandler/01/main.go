b5172c26fcc137aa79b43e5fff8f9e9f38631f5f
Chidiebere Okpala <okpalacollins4@gmail.com>
2021-04-18 20:48:31 -0600

    The http.NotFoundHandler method

--- 025_NotFoundHandler/01/main.go
+++ 025_NotFoundHandler/01/main.go
@@ -7,6 +7,15 @@ import (
 
 func main() {
 	http.HandleFunc("/", foo)
+	/*The browser like google chrome will often ask you for your favicon.ico. SOme browser don't ask for it
+	faveicon.com is the little image that shows on the tab of the browser for the site page you are on
+	If you dont have the faveicon you want to be able to respond back to the browser request that you dont have one with like a 404 code
+
+	So, in our case, we passed the NotFoundHandler() method which does that for us.
+
+	http.NotFoundHandler() return a Handler thats why we can pass it to the Handle() method
+	https://golang.org/pkg/net/http/#NotFoundHandler
+	*/
 	http.Handle("/favicon.ico", http.NotFoundHandler())
 	http.ListenAndServe(":8080", nil)
 }
