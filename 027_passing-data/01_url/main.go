4e7a4713868351557c66bce4d8f0358d8f5e9d62
Chidiebere Okpala <okpalacollins4@gmail.com>
2021-05-05 17:32:06 -0600

    Passing Form values through the url
    We used the FormValue method to get values from our url in the server

--- 027_passing-data/01_url/main.go
+++ 027_passing-data/01_url/main.go
@@ -12,8 +12,8 @@ func main() {
 }
 
 func foo(w http.ResponseWriter, req *http.Request) {
-	v := req.FormValue("q")
-	fmt.Fprintln(w, "Do my search: "+v)
+	v := req.FormValue("q")             //FormValue method is used to get each variable passed from the url. In this case "q" is the variable which has the value "dog"
+	fmt.Fprintln(w, "Do my search: "+v) //we will this string in the web browser "Do my search: dog"
 }
 
 // visit this page:
