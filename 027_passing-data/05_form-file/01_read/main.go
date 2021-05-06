627c184464c0f77170428280f90653e3a1ee27a1
Chidiebere Okpala <okpalacollins4@gmail.com>
2021-05-05 19:51:47 -0600

    Uploading a file, Reading the file & creating a file on the server
    We have a form that allows a user to upload a file, then we wrote a code on our server so that when that data gets to the server, we can deal with that file that was uploaded

--- 027_passing-data/05_form-file/01_read/main.go
+++ 027_passing-data/05_form-file/01_read/main.go
@@ -13,25 +13,32 @@ func main() {
 	http.ListenAndServe(":8080", nil)
 }
 
+/*
+We get open the file that was submitted by the user, read it and then write the content
+of the file back to the response
+*/
 func foo(w http.ResponseWriter, req *http.Request) {
 
 	var s string
 	fmt.Println(req.Method)
+	//we are checking if the request method is POST. NOTE that http.MethodPost is a constant
+	//https://golang.org/pkg/net/http/#pkg-constants
 	if req.Method == http.MethodPost {
 
 		// open
-		f, h, err := req.FormFile("q")
+		f, h, err := req.FormFile("q") //FromFile method is used get form input file values! . The identifier for this file value is "q" Remember how we use FormValue method to get Form input text values
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 		defer f.Close()
+		//Side NOTE: you can Reac, Seek and Close this file. If you how to seek the file, you have to use te Seeker type
 
 		// for your information
 		fmt.Println("\nfile:", f, "\nheader:", h, "\nerr", err)
 
 		// read
-		bs, err := ioutil.ReadAll(f)
+		bs, err := ioutil.ReadAll(f) //file has the reader interface, this is why we can pass it to ReadAll method
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
@@ -39,11 +46,18 @@ func foo(w http.ResponseWriter, req *http.Request) {
 		s = string(bs)
 	}
 
+	/*
+		This block of code will run whether it is a POST or GET request coming in
+
+		NOTE: we did not specify an action url for this form we are displaing.
+		This means it will post the action to this page where the request is coming from /
+		We dont have to specify the action attribute if we want the form to be subitted at thesame page!
+	*/
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
 	io.WriteString(w, `
 	<form method="POST" enctype="multipart/form-data">
 	<input type="file" name="q">
 	<input type="submit">
 	</form>
-	<br>`+s)
+	<br>`+s) //The "s" variable we write back as request will either contain a zero value or content of the file the user uploaded
 }
--- 027_passing-data/05_form-file/02_store/main.go
+++ 027_passing-data/05_form-file/02_store/main.go
@@ -21,6 +21,10 @@ func main() {
 	http.ListenAndServe(":8080", nil)
 }
 
+/*
+This time we open the file uploaded, read the file and write the
+contents of the uploaded file to another file and save it in our server
+*/
 func foo(w http.ResponseWriter, req *http.Request) {
 
 	var s string
@@ -43,17 +47,18 @@ func foo(w http.ResponseWriter, req *http.Request) {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
-		s = string(bs)
+		s = string(bs) //we convert our byte slice to a string. The return the content of the file uploaded in the request and print in the webpage
 
 		// store on server
-		dst, err := os.Create(filepath.Join("./user/", h.Filename))
+		dst, err := os.Create(filepath.Join("./user/", h.Filename)) //we created a file in of name ./user/<NAME_OF_THE_FILE_USER_UPLOADED>
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
 		defer dst.Close()
 
-		_, err = dst.Write(bs)
+		//ds varible is a pointer to the file we created, so we can write directly to that file! We call the Write() method on that variable
+		_, err = dst.Write(bs) //We write the byte slice(content of the uploaded file) to the new file we created
 		if err != nil {
 			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
