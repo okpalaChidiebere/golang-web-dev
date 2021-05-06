91aa559408d658ea817be6960d0ac6c1d3f0ce9b
Chidiebere Okpala <okpalacollins4@gmail.com>
2021-05-05 20:19:37 -0600

    Enctype

--- 027_passing-data/06_enctype/01_default/main.go
+++ 027_passing-data/06_enctype/01_default/main.go
@@ -27,9 +27,9 @@ func main() {
 func foo(w http.ResponseWriter, req *http.Request) {
 
 	// body
-	bs := make([]byte, req.ContentLength)
-	req.Body.Read(bs)
-	body := string(bs)
+	bs := make([]byte, req.ContentLength) //we make a slice of byte of size that a length of the request
+	req.Body.Read(bs)                     //we read all of our request body into that byte slice
+	body := string(bs)                    //turn the byte slice into a string which wil be the part the body of the response we wil write back to the user
 
 	err := tpl.ExecuteTemplate(w, "index.gohtml", body)
 	if err != nil {
