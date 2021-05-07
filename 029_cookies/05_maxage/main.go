2dd714f2324c57bce186454fba61fd5de65c8f7e
Chidiebere Okpala <okpalacollins4@gmail.com>
2021-05-07 15:57:06 -0600

    Deleting a cookie

--- 029_cookies/05_maxage/main.go
+++ 029_cookies/05_maxage/main.go
@@ -6,7 +6,7 @@ import (
 )
 
 func main() {
-	http.HandleFunc("/", index)
+	http.HandleFunc("/", index) //loads the home page with a link to the 'set' route
 	http.HandleFunc("/set", set)
 	http.HandleFunc("/read", read)
 	http.HandleFunc("/expire", expire)
@@ -21,16 +21,16 @@ func set(w http.ResponseWriter, req *http.Request) {
 	http.SetCookie(w, &http.Cookie{
 		Name:  "session",
 		Value: "some value",
-		Path: "/",
+		Path:  "/",
 	})
 	fmt.Fprintln(w, `<h1><a href="/read">read</a></h1>`)
 }
 
 func read(w http.ResponseWriter, req *http.Request) {
 	c, err := req.Cookie("session")
-	if err != nil {
-		http.Redirect(w, req, "/set", http.StatusSeeOther)
-		return
+	if err != nil { //at this point, it means there is no cookie
+		http.Redirect(w, req, "/set", http.StatusSeeOther) //we just redirect the user back to the set route. Yey! here we go again with 303
+		return                                             //we dont forget to include the return statement
 	}
 
 	fmt.Fprintf(w, `<h1>Your Cookie:<br>%v</h1><h1><a href="/expire">expire</a></h1>`, c)
@@ -40,9 +40,9 @@ func expire(w http.ResponseWriter, req *http.Request) {
 	c, err := req.Cookie("session")
 	if err != nil {
 		http.Redirect(w, req, "/set", http.StatusSeeOther)
-		return
+		return //we must return, so that we dont go ahead to delete a cookie when there is no cookie!
 	}
-	c.MaxAge = -1 // delete cookie
-	http.SetCookie(w, c)
-	http.Redirect(w, req, "/", http.StatusSeeOther)
+	c.MaxAge = -1                                   // delete cookie. NOTE: You can set the age to 0 or a negative value. They are all thesame. It will dlete the cookie
+	http.SetCookie(w, c)                            //we need to seek the cookie as we hav change one of the fields
+	http.Redirect(w, req, "/", http.StatusSeeOther) //we redirect back to the default route
 }
