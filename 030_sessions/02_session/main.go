a53a4444a044b79d090f90b1263209d3c9989cf0
Chidiebere Okpala <okpalacollins4@gmail.com>
2021-05-07 17:37:32 -0600

    Creating Sessions
    Clients makes request to the server, and we want the client to send a unique identifier (sessionID) to that server. In our server, we could associated that SessionID to a UserID (we have a table that stores a sessionID for every userID). Then we can now take the userID and get anything we want about the user. We could store that Unique ID(sessionID) in a cookie and knowing that cookie are domain specific, whenever the client send a request to our server, if there is a our cookie on their browser, they will send that cookie as well. Then our server can grab that value and we can uniquely identify them.
    
    The SessionID are stored and deleted as the user logs in and out from our server. But the UserID and other information is always there in our DB table

--- 030_sessions/02_session/main.go
+++ 030_sessions/02_session/main.go
@@ -1,8 +1,8 @@
 package main
 
 import (
-	"github.com/satori/go.uuid"
 	"html/template"
+	//"github.com/satori/go.uuid"
 	"net/http"
 )
 
@@ -15,6 +15,7 @@ type user struct {
 var tpl *template.Template
 var dbUsers = map[string]user{}      // user ID, user
 var dbSessions = map[string]string{} // session ID, user ID
+//an aletrnative way we could have initialize an empty map is to use the make method. eg make(map[string]string) instead of map[string]string{}
 
 func init() {
 	tpl = template.Must(template.ParseGlob("templates/*"))
@@ -30,8 +31,8 @@ func main() {
 func index(w http.ResponseWriter, req *http.Request) {
 
 	// get cookie
-	c, err := req.Cookie("session")
-	if err != nil {
+	c, err := req.Cookie("session") //we ask for a cookie
+	if err != nil {                 //if the cookie is not there(set in the client machine, and sent with this request), we create one
 		sID, _ := uuid.NewV4()
 		c = &http.Cookie{
 			Name:  "session",
@@ -42,8 +43,15 @@ func index(w http.ResponseWriter, req *http.Request) {
 
 	// if the user exists already, get user
 	var u user
+	/*
+		c.Value s the value of the cookie, but for the map, the cookie value is
+		the key that will give us the username.
+		The username according to our app must be unique. But ideally we will user a userID instead of name
+
+		The username is now a key for the users map table "dbUsers"
+	*/
 	if un, ok := dbSessions[c.Value]; ok {
-		u = dbUsers[un]
+		u = dbUsers[un] //  we now get the username of the user by id from the user table
 	}
 
 	// process form submission
@@ -51,23 +59,32 @@ func index(w http.ResponseWriter, req *http.Request) {
 		un := req.FormValue("username")
 		f := req.FormValue("firstname")
 		l := req.FormValue("lastname")
-		u = user{un, f, l}
-		dbSessions[c.Value] = un
+		u = user{un, f, l}       //we create a new user
+		dbSessions[c.Value] = un //we associate the sessionId with the username
 		dbUsers[un] = u
 	}
 
 	tpl.ExecuteTemplate(w, "index.gohtml", u)
 }
 
+/*
+With this function, if there is a session, we display the username of the user logged in
+
+Remember the username is associated to a session in this app
+*/
 func bar(w http.ResponseWriter, req *http.Request) {
 
 	// get cookie
 	c, err := req.Cookie("session")
 	if err != nil {
-		http.Redirect(w, req, "/", http.StatusSeeOther)
+		http.Redirect(w, req, "/", http.StatusSeeOther) //if there is no session, we just redirect back to the default route and not print anything
 		return
 	}
 	un, ok := dbSessions[c.Value]
+	/*
+		If the user session has a username attached to it(something we stored),
+		ok value will be true and we will not run the redirect code
+	*/
 	if !ok {
 		http.Redirect(w, req, "/", http.StatusSeeOther)
 		return
