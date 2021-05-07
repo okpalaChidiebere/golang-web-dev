65cee53aae9399aecb1878850bcce3d1e3da6c56
Chidiebere Okpala <okpalacollins4@gmail.com>
2021-05-07 17:00:48 -0600

    Storing a universally unique identifier into a cookie
    Remember an alternative way to manage state is pass the uniqueId into evety internal url as a url params on your web page. You dont want to pass that id to another domain

--- 030_sessions/01_uuid/main.go
+++ 030_sessions/01_uuid/main.go
@@ -2,7 +2,7 @@ package main
 
 import (
 	"fmt"
-	"github.com/satori/go.uuid"
+	//"github.com/satori/go.uuid"
 	"net/http"
 )
 
@@ -18,15 +18,17 @@ func main() {
 func index(w http.ResponseWriter, req *http.Request) {
 	cookie, err := req.Cookie("session")
 	if err != nil {
-		id := uuid.NewV4()
+		// If there is an error, we have no cookie; so we create a new one
+
+		id := uuid.NewV4() //ideally, the package name is thesame as the folder name, but satori went agains the rule :) The foldername is "go.uuid" and package name is "uuid"
 		cookie = &http.Cookie{
 			Name:  "session",
-			Value: id.String(),
-			// Secure: true,
-			HttpOnly: true,
-			Path:     "/",
+			Value: id.String(), //we take the uuid and turn it to a string
+			// Secure: true, //if we were using HTTPS, we will uncomment this line. This allows the cookie to be sent only on https securely
+			HttpOnly: true, //HTTPOnly means that you cant access this cookie with JavaScript. This makes this cookie even more secure
+			Path:     "/",  //we set th path to be default which allows the cookie to be accessed all over our website. But we can specify a special subpath as well eh /auth/blaBla
 		}
 		http.SetCookie(w, cookie)
 	}
-	fmt.Println(cookie)
+	fmt.Println(cookie) //we print the cookie on log server if there is one!
 }
