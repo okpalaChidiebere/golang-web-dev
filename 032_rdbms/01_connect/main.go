c36b0befc55f7566f8b3637f6184e48c1bc0c907
Chidiebere Okpala <okpalacollins4@gmail.com>
2021-05-10 13:20:30 -0600

    GO & SQL setup
    Firstly, be sure to download MySQL server and WorkBench from his link if you don't have one in our system.
    We created a new dtatabse(called schema) and added tables to it with few records added to the table all from the MySQL workbench!
    
    Provision your SQL database at AWS.
    There are things to note as you Provision the database just for learning and not Prod or dev
    - Make sure you select the Free-tier. By selecting this, AWS will disable some other form fields that you dont need to configure because of free tier. Leave the defaults!
    - Make your DB instance name, database name and your username thesame. This is just for simplicity and good practise.
    - Make your db publicly accessible because we will want to be connecting from our local machine mysql workbench using the assigned url give to us by AWS for this resource. For Prod, you may not want to do this though
    - Select the VPC. You can create a new one or use an existing one. Either way, you will need to set the VPC to allow connections from anywhere. On prod or dev, it is good practise to limit the access by listing out the IP address that can only connect to the database
    - Be Sure to enable back up, monitoring and also deletion protection
    
    In our code below, we just tested out the connection to the database. And it works fine!

--- 032_rdbms/01_connect/main.go
+++ 032_rdbms/01_connect/main.go
@@ -3,9 +3,19 @@ package main
 import (
 	"database/sql"
 	"fmt"
-	_ "github.com/go-sql-driver/mysql"
+
+	/*
+		The reason we have the blanc identifier "_" is we are importing the driver
+		But we are not really using anything from this package. This package is
+		imported strictly for set up. The code from this driver package will be analyzied
+		only when our mysql connection is being made for the drivers to get connected
+
+		For our app, the driver is mysql. You can see that passed as the first parameter in sql.Open() method
+	*/
 	"io"
 	"net/http"
+
+	_ "github.com/go-sql-driver/mysql"
 )
 
 var db *sql.DB
@@ -13,7 +23,7 @@ var err error
 
 func main() {
 	// user:password@tcp(localhost:5555)/dbname?charset=utf8
-	db, err = sql.Open("mysql", "awsuser:mypassword@tcp(mydbinstance.cakwl95bxza0.us-west-1.rds.amazonaws.com:3306)/test02?charset=utf8")
+	db, err = sql.Open("mysql", "awsuser:mypassword@tcp(mydbinstance.cakwl95bxza0.ca-central-1.rds.amazonaws.com:3306)/test02?charset=utf8")
 	check(err)
 	defer db.Close()
 
