b1ac9f7ec6e2685aed3814ce22c2211832e4872b
Chidiebere Okpala <okpalacollins4@gmail.com>
2021-05-13 11:31:21 -0600

    Web Dev Toolkit - TLS & HTTPS

--- 039_https/01/main.go
+++ 039_https/01/main.go
@@ -6,7 +6,7 @@ import (
 
 func main() {
 	http.HandleFunc("/", foo)
-	http.ListenAndServeTLS(":10443", "cert.pem", "key.pem", nil)
+	http.ListenAndServeTLS(":10443", "cert.pem", "key.pem", nil) //NOTE: we server HTTPS at port 443 for prod
 }
 
 func foo(w http.ResponseWriter, req *http.Request) {
@@ -30,3 +30,13 @@ func foo(w http.ResponseWriter, req *http.Request) {
 // instead of go env GOROOT
 // you can just use the path to the GO SDK
 // wherever it is on your computer
+
+/*NOTE the host you want to get your key cert and private key should be from a legit certificate authority approved by web like https://letsencrypt.org/ or
+you can find our names of authorized companies that offer TLS encryption that your browser approves of
+
+To get the list of companies, go to this link chrome://settings/security
+then Manage certificates, it will open a keychain Access app. In the app, go to "system Roots" and you will see the list of companies that server TLS certificates or HTTPS
+The companies charge you a handful though :(
+
+	Extra: https://caniuse.com/http2
+*/
