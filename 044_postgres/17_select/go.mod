02f9f1db9c2343c9b4eb2c244777ec88afd295a9
Chidiebere Okpala <okpalacollins4@gmail.com>
2021-05-14 16:31:37 -0600

    Go & Postgres - Select Query
    We created our table(called books) in the database(called bookstore), populated records to it and pulled data out of the table

---
+++ 044_postgres/17_select/go.mod
@@ -0,0 +1,5 @@
+module select-query/m
+
+go 1.16
+
+require github.com/lib/pq v1.10.1
--- 044_postgres/17_select/main.go
+++ 044_postgres/17_select/main.go
@@ -3,10 +3,13 @@ package main
 import (
 	"database/sql"
 	"fmt"
+	"os"
+
 	_ "github.com/lib/pq"
 )
 
 type Book struct {
+	//NOTE: the order and the names of theses fields must match what you have in the database. This is IMPORTANT
 	isbn   string
 	title  string
 	author string
@@ -14,36 +17,43 @@ type Book struct {
 }
 
 func main() {
-	db, err := sql.Open("postgres", "postgres://bond:password@localhost/bookstore?sslmode=disable")
+	un := os.Getenv("POSTGRES_USERNAME") //FYI: this user must have the "SUPERUSER" role in postgress to do this operations https://www.postgresql.org/docs/12/sql-alteruser.html
+	pwd := os.Getenv("POSTGRES_PASSWORD")
+	h := os.Getenv("POSTGRES_HOST")
+	pdb := os.Getenv("POSTGRES_DATABASE")
+
+	db, err := sql.Open("postgres", "postgres://"+un+":"+pwd+"@"+h+"/"+pdb+"?sslmode=disable")
 	if err != nil {
 		panic(err)
 	}
-	defer db.Close()
+	defer db.Close() //we defer close that pointer to db
 
 	if err = db.Ping(); err != nil {
 		panic(err)
 	}
 	fmt.Println("You connected to your database.")
 
-	rows, err := db.Query("SELECT * FROM books;")
+	//We created books table and populated it with a few books already using postbird
+	rows, err := db.Query("SELECT * FROM books;") //NOTE: the semi-colon must be at the end of the SQL stement
 	if err != nil {
 		panic(err)
 	}
-	defer rows.Close()
+	defer rows.Close() //we defer close that pointer to rows
 
-	bks := make([]Book, 0)
+	bks := make([]Book, 0) //the 0 means we length of the slice. it is not a fixed length
 	for rows.Next() {
 		bk := Book{}
 		err := rows.Scan(&bk.isbn, &bk.title, &bk.author, &bk.price) // order matters
 		if err != nil {
 			panic(err)
 		}
-		bks = append(bks, bk)
+		bks = append(bks, bk) //we add the book to our slice of book
 	}
 	if err = rows.Err(); err != nil {
 		panic(err)
 	}
 
+	//we range over the slice
 	for _, bk := range bks {
 		// fmt.Println(bk.isbn, bk.title, bk.author, bk.price)
 		fmt.Printf("%s, %s, %s, $%.2f\n", bk.isbn, bk.title, bk.author, bk.price)
